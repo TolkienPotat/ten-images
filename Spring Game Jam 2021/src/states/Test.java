222f34728e5dbbe61427d583311739106869a7ae
shae <shae.d.mckeever@gmail.com>
2021-03-04 20:57:28 -0800

    Ignore this

--- Spring Game Jam 2021/src/states/Test.java
+++ Spring Game Jam 2021/src/states/Test.java
@@ -29,7 +29,7 @@ public class Test implements State {
 	public void init() {
 		map = new Map();
         map.loadMapFile("DefaultResources/Files/test-map.map", 30, 30);
-        player = new Player(Texture.loadTexture("DefaultResources/Images/Pllayer.png"));
+        player = new Player(Texture.loadTexture("DefaultResources/Images/pLAYER.png"));
         camera = new Camera(0, 0);
         
         player.inGameX = 1920/2;
